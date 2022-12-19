d4004443c4aaf5237dc0f31e99d874af43781d71
roro03tv <60408114+roro03tv@users.noreply.github.com>
2022-12-19 20:47:55 +0100

    Delete Ejercicio11.py

--- Ejercicio11.py
+++
@@ -1,9 +0,0 @@
-capital = float(input("Introduce el capital de la inversión: "))
-interes = 0.04
-primer = round(float(capital + capital*interes), 2)
-segundo = round(float(primer + primer*interes), 2)
-tercer = round(float(segundo + segundo*interes), 2)
-
-print("Después del primer año los ahorros son de: " + str(primer))
-print("Después del segundo año los ahorros son de: " + str(segundo))
-print("Después del tercer año los ahorros son de: " + str(tercer))
